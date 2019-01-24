00a4268bb68b661e769e75792fcc5b3c125d6481
Llight9 <lpcm501@york.ac.uk>
2019-01-24 12:49:48 +0000

    added led test values

--- lcd_menu-test.c
+++ lcd_menu-test.c
@@ -1,19 +1,25 @@
 #include "lcd_menu.h"
 
 void main (){
+    setup_leds();
+    write_leds(1);
     char *string_database[5]={'\0'};
     string_database[0]="Florida";
     string_database[1]="Oregon";
     string_database[2]="California";
     string_database[3]="Georgia";
     int i;
+    write_leds(2);
     menu_make(3, string_database);
+    write_leds(3);
     for (i = 0; i < 10; i ++){
         Delay(400);
         menu_down();
     }
+    write_leds(4);
     for (i = 0; i < 10; i ++){
         Delay(400);
         menu_up();
     }
+    write_leds(5);
 }
