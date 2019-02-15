f4f7719c28d73c63e47f1a0b29552b0c2a70284e
Llight9 <lpcm501@york.ac.uk>
2019-02-15 13:51:45 +0000

    spi read config added

--- spiIO.c
+++ spiIO.c
@@ -11,26 +11,48 @@
 #define CS_PORT_NUM             0
 #define CS_PIN_NUM              11
 
-#define MBEDAUDIO_REG_LMIC                  0x00
-#define MBEDAUDIO_REG_RMIC                  0x200
-#define MBEDAUDIO_REG_LOUT                  0x400
-#define MBEDAUDIO_REG_ROUT                  0x600
-#define MBEDAUDIO_REG_ANALOGPATH            0x800
-#define MBEDAUDIO_REG_DIGITALPATH           0xA00
-#define MBEDAUDIO_REG_POWER                 0xC00
-#define MBEDAUDIO_REG_DIGITALINTERFACE      0xE00
-#define MBEDAUDIO_REG_SAMPLERATE            0x1000
-#define MBEDAUDIO_REG_DIGITALACTIVATION     0x1200
-#define MBEDAUDIO_REG_RESET                 0x1E00
+//Controls left input channel volume 0x00
+#define MBEDAUDIO_REG_LMIC  0x00
+#define LMIC_VALUE 0x1F //000011111
+//Controls right input channel volume 0x01
+#define MBEDAUDIO_REG_RMIC  0x200
+#define RMIC_VALUE 0x1F //000011111
+//Controls left headphone channel volume 0x02
+#define MBEDAUDIO_REG_LOUT  0x400
+#define LOUT_VALUE 0xFF //011111111
+//Controls right headphone channel volume 0x03
+#define MBEDAUDIO_REG_ROUT  0x600
+#define ROUT_VALUE 0xFF //011111111
+//Controls the analog path register 0x04
+#define MBEDAUDIO_REG_ANALOGPATH  0x800
+#define ANALOGPATH_VALUE 0x15 //000010101 //if anything is wrong its probably this
+//Controls the digital path register 0x05
+#define MBEDAUDIO_REG_DIGITALPATH  0xA00
+#define DIGITALPATH_VALUE 0x04 //000000100
+//Controls power to various components i.e. DAC 0x06
+#define MBEDAUDIO_REG_POWER  0xC00
+#define POWER_VALUE 0x07 //000000111
+//Controls the digital interface path i.e. filters 0x07
+#define MBEDAUDIO_REG_DIGITALINTERFACE  0xE00
+#define DIGITALINTERFACE_VALUE 0x2 //000000010
+//Controls the sample rate 0x08
+#define MBEDAUDIO_REG_SAMPLERATE  0x1000
+#define SAMPLERATE_VALUE 0x0C //000001100
+//Enables various digital components 0x09
+#define MBEDAUDIO_REG_DIGITALACTIVATION  0x1200
+#define DIGITALACTIVATION_VALUE 0x1 //0000000001
+//Resets all registers to their default state 0x0F
+#define MBEDAUDIO_REG_RESET 0x1E00
 
-void spi_write(uint16_t buff[], int len);
-void spi_bypass(void);
-uint32_t sd_polling(void* tx_buf, void* rx_buf, uint32_t length);
-void CS_Force(int32_t state);
+PINSEL_CFG_Type PinCfg;
+
+SPI_DATA_SETUP_Type xferConfig;
+
 void CS_Force(int32_t state);
 uint32_t sd_polling(void* tx_buf, void* rx_buf, uint32_t length);
 void spi_init(void);
 void spi_bypass(void);
+void spi_init_read(void);
 void spi_write(uint16_t buff[], int len);
 
 void CS_Force(int32_t state){
@@ -84,9 +106,26 @@ void spi_bypass(void){
     spi_write(transmitbuffer, 9);
 }
 
+void spi_init_read(void){
+	uint16_t transmitbuffer[11] = {(MBEDAUDIO_REG_RESET|0x0),
+				 (MBEDAUDIO_REG_LMIC|LMIC_VALUE),
+				 (MBEDAUDIO_REG_RMIC|RMIC_VALUE),
+				 (MBEDAUDIO_REG_LOUT|LOUT_VALUE),
+				 (MBEDAUDIO_REG_ROUT|ROUT_VALUE),
+				 (MBEDAUDIO_REG_ANALOGPATH|ANALOGPATH_VALUE),
+				 (MBEDAUDIO_REG_DIGITALPATH|DIGITALPATH_VALUE),
+				 (MBEDAUDIO_REG_POWER|POWER_VALUE),
+				 (MBEDAUDIO_REG_DIGITALINTERFACE|DIGITALINTERFACE_VALUE),
+				 (MBEDAUDIO_REG_SAMPLERATE|SAMPLERATE_VALUE),
+				 (MBEDAUDIO_REG_DIGITALACTIVATION|DIGITALACTIVATION_VALUE)
+    };
+    spi_write(transmitbuffer, 11);
+}
+
 void spi_write(uint16_t buff[], int len){
     int i;
     for (i = 0; i < len; ++i){
         sd_polling(&buff[i], NULL, 2);
     }
 }
+
--- spiIO.h
+++ spiIO.h
@@ -7,20 +7,36 @@
 #define CS_PORT_NUM             0
 #define CS_PIN_NUM              11
 
-#define MBEDAUDIO_REG_LMIC                  0x00
-#define MBEDAUDIO_REG_RMIC                  0x200
-#define MBEDAUDIO_REG_LOUT                  0x400
-#define MBEDAUDIO_REG_ROUT                  0x600
-#define MBEDAUDIO_REG_ANALOGPATH            0x800
-#define MBEDAUDIO_REG_DIGITALPATH           0xA00
-#define MBEDAUDIO_REG_POWER                 0xC00
-#define MBEDAUDIO_REG_DIGITALINTERFACE      0xE00
-#define MBEDAUDIO_REG_SAMPLERATE            0x1000
-#define MBEDAUDIO_REG_DIGITALACTIVATION     0x1200
-#define MBEDAUDIO_REG_RESET                 0x1E00
+#define MBEDAUDIO_REG_LMIC  0x00
+#define LMIC_VALUE 0x1F //000011111
+#define MBEDAUDIO_REG_RMIC  0x200
+#define RMIC_VALUE 0x1F //000011111
+#define MBEDAUDIO_REG_LOUT  0x400
+#define LOUT_VALUE 0xFF //011111111
+#define MBEDAUDIO_REG_ROUT  0x600
+#define ROUT_VALUE 0xFF //011111111
+#define MBEDAUDIO_REG_ANALOGPATH  0x800
+#define ANALOGPATH_VALUE 0x15 //000010101 //if anything is wrong its probably this
+#define MBEDAUDIO_REG_DIGITALPATH  0xA00
+#define DIGITALPATH_VALUE 0x04 //000000100
+#define MBEDAUDIO_REG_POWER  0xC00
+#define POWER_VALUE 0x07 //000000111
+#define MBEDAUDIO_REG_DIGITALINTERFACE  0xE00
+#define DIGITALINTERFACE_VALUE 0x2 //000000010
+#define MBEDAUDIO_REG_SAMPLERATE  0x1000
+#define SAMPLERATE_VALUE 0x0C //000001100
+#define MBEDAUDIO_REG_DIGITALACTIVATION  0x1200
+#define DIGITALACTIVATION_VALUE 0x1 //0000000001
+#define MBEDAUDIO_REG_RESET 0x1E00
+
+PINSEL_CFG_Type PinCfg;
+
+SPI_DATA_SETUP_Type xferConfig;
 
 void spi_init(void);
 
 void spi_bypass(void);
+
+void spi_init_read(void);
 
 void spi_write(uint16_t buff[], int len);
