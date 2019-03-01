627322e1a7f6c07f482daaebd79be3b284998a11
Llight9 <lpcm501@york.ac.uk>
2019-03-01 17:05:55 +0000

    menu_tree.h made

---
+++ menu_tree.h
@@ -0,0 +1,51 @@
+#define audio_type_in         1
+#define audio_type_out        2
+#define audio_type_bypass     3
+
+#define menu_type_list        1
+#define menu_type_selc        2
+#define menu_type_opt         3
+#define menu_type_multi_opt   4
+
+typedef struct node {
+    node * parent;
+    int child_num;
+    int data_num;
+    char * data[data_num];
+    int data_item_len[data_num];
+    int data_type;
+    node * children[child_num];
+} node;
+
+typedef struct effect_data {
+    int speed;
+    int pitch_shift;
+} effect_data;
+
+typedef struct format_data {
+    int loss;
+} format_data;
+
+typedef struct quality_data {
+    int quality
+} quality_data;
+
+typedef struct quality_data {
+    int quality
+} quality_data;
+
+typedef struct menu_status {
+    int audio_type;
+    format_data format_type;
+    quality_data quality_type;
+    effect_data effect_type;
+} menu_status;
+
+void menu_init(void);
+void menu_up(void);
+void menu_down(void);
+void menu_forward();
+void menu_back();
+menu_status menu_select(void);
+int menu_type(void);
+void exit_menu(void);
